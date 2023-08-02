17170ff3dbc1c44c75b0b89be8194e85c67f9999
JeryRPG <nealthmc@gmail.com>
2023-08-02 15:56:11 -0500

    pull up method and field

--- Taller Refactoring/src/EmpleadoPorHoras.java
+++ Taller Refactoring/src/EmpleadoPorHoras.java
@@ -1,18 +1,8 @@
 public class EmpleadoPorHoras extends Empleado {
-    private String genero;
 
     public EmpleadoPorHoras(String nombre, int horasTrabajadas, double tarifaHora, String departamento, String genero) {
-        super(nombre, 0, horasTrabajadas, tarifaHora, departamento);
+        super(nombre, 0, horasTrabajadas, tarifaHora, departamento, genero);
         super.setSalarioBase(calcularSalario());
-        this.genero = genero;
-    }
-
-    public void imprimirDetalles() {
-        System.out.println("Nombre: " + super.getNombre());
-        System.out.println("Genero: " + super.getNombre());
-        System.out.println("Salario: " + super.getSalarioBase());
-        System.out.println("Horas trabajadas: " + super.getHorasTrabajadas());
-        System.out.println("Departamento: " + super.getDepartamento());
     }
 
     @Override
